4d9d28f487e09b91caa1d3346aac2f10e89c536a
ak6130 <albert.polletta@gmail.com>
2020-10-22 15:06:02 +0200

    Update _foot.php

--- inc/_foot.php
+++ inc/_foot.php
@@ -8,9 +8,9 @@
 		Powered by <a href='http://processwire.com'>ProcessWire CMS</a> and <a href="http://snipcart.com">Snipcart</a>  &nbsp; / &nbsp; 
 		</p>
 	</footer>
-<!-- 
+ 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
 <?php
