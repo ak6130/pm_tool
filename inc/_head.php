829e4b90e73f11e8dfecdd8060663bb10658946c
ak6130 <albert.polletta@gmail.com>
2020-10-22 14:47:27 +0200

    Update _head.php

--- inc/_head.php
+++ inc/_head.php
@@ -7,9 +7,10 @@
 	<meta name="description" content="<?php echo $page->summary; ?>" />
 	<link rel="preconnect" href="https://app.snipcart.com">
 	<link rel="preconnect" href="https://cdn.snipcart.com">
+	<link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.23/default/snipcart.css" />
 	<link href='//fonts.googleapis.com/css?family=Lusitana:400,700|Quattrocento:400,700' rel='stylesheet' type='text/css' />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.23/default/snipcart.css" />
+	
 </head>
 <body class='has-sidebar'>
 
