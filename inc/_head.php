03f05ac3556741835dc4e5ac789fc2de173d0608
ak6130 <albert.polletta@gmail.com>
2020-10-22 14:39:51 +0200

    bootstrap me

--- inc/_head.php
+++ inc/_head.php
@@ -8,6 +8,7 @@
 	<link rel="preconnect" href="https://app.snipcart.com">
 	<link rel="preconnect" href="https://cdn.snipcart.com">
 	<link href='//fonts.googleapis.com/css?family=Lusitana:400,700|Quattrocento:400,700' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.0.23/default/snipcart.css" />
 </head>
 <body class='has-sidebar'>
