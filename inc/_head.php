ea4ae343880505499557100eb9ac2f6ba1175833
ak6130 <albert.polletta@gmail.com>
2020-10-23 15:14:03 +0200

    Update _head.php

--- inc/_head.php
+++ inc/_head.php
@@ -14,20 +14,19 @@
 </head>
 <body>
 
-	<!-- top navigation 
-	<ul class='topnav'><?php
+	<?php
 
 		// top navigation consists of homepage and its visible children
 		$homepage = $pages->get('/');
 		$children = $homepage->children();
 
 		// make 'home' the first item in the navigation
-		$children->prepend($homepage);
+		// $children->prepend($homepage);
 
-	?></ul> -->
+	?>
 
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  <a class="navbar-brand" href="#">Navbar</a>
+	  <a class="navbar-brand" href="#">Schwaz IT - lokal, online, nachhaltig </a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
@@ -35,7 +34,7 @@
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item active">
-	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	        <a class="nav-link" href="<?=$homepage->url?>">Home <span class="sr-only">(current)</span></a>
 	      </li>
 	      <li class="nav-item">
 	        <a class="nav-link" href="#">Link</a>
