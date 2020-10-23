c3d7e2fdf0fc91c71da619d224153aecd1ad20cf
ak6130 <albert.polletta@gmail.com>
2020-10-23 16:57:15 +0200

    grid fix

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
 		$rowCount = 0; 
 		$bootstrapColWidth = 12 / $numofCols;
 		foreach($page->children() as $product) {
-			if($rowCount % $numofCols == 0) { echo "<div class='row'>";}
+			if($rowCount % $numofCols == 0) { echo "<div class='row mt-2'>";}
 			$rowCount++;
 			$image = $product->product_image->url;
 			$render = "<div class='col-md-" . $bootstrapColWidth . "'>"
